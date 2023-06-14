c8c58aab3ebeab60b5a97c26ff958dcd786c946b
truonghvph16694 <96322678+truonghvph16694@users.noreply.github.com>
2023-06-14 14:31:51 +0700

    connect db

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 
 dotenv.config()
 const app = express();
 
 const port = process.env.PORT || 8000;
+
+app.get("/",(req,res) =>{
+    res.send("Hello World")
+})
+
+//Kết nối cơ sở dữ liệu với mongoose alat
+mongoose.connect(`mongodb+srv://truonghvph16694:${process.env.MONGO_DB}@cluster0.eux1ygq.mongodb.net/`)
+.then(() =>{
+    console.log('connect Db success!!');
+})
+.catch((err) =>{
+    console.log('Error', err)
+})
+
 app.listen(port , () =>{
     console.log('Service is running on port', port);
 })
