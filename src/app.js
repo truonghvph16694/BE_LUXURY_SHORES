58e6dc3323815dae23e88d0fe109ffbe1c2f0d01
Tuanvmph16643 <106154068+Tuanvmph16643@users.noreply.github.com>
2023-06-14 23:03:25 +0700

    order
    crud order

--- src/app.js
+++ src/app.js
@@ -1,26 +1,54 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser';
+import cors from "cors";
 import mongoose from 'mongoose';
 
+import routerOrders from './routers/routers';
+import morgan from 'morgan';
 
 dotenv.config()
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"))
+
+app.use("/api",routerOrders)
+
+mongoose.connect("mongodb://127.0.0.1:27017/datn");
 
 const port = process.env.PORT || 8000;
-
-app.get("/",(req,res) =>{
-    res.send("Hello World")
-})
-
-//Kết nối cơ sở dữ liệu với mongoose alat
-mongoose.connect(`mongodb+srv://truonghvph16694:${process.env.MONGO_DB}@cluster0.eux1ygq.mongodb.net/`)
-.then(() =>{
-    console.log('connect Db success!!');
-})
-.catch((err) =>{
-    console.log('Error', err)
-})
-
 app.listen(port , () =>{
     console.log('Service is running on port', port);
 })
+// asdiuw
+
+// import express from 'express'
+// import dotenv from 'dotenv'
+// import mongoose from 'mongoose';
+// import routerOrders from './routers/routers';
+
+
+// dotenv.config()
+// const app = express();
+
+// const port = process.env.PORT || 8000;
+
+// app.get("/",(req,res) =>{
+//     res.send("Hello World")
+// })
+
+// app.use("/api",routerOrders)
+
+// //Kết nối cơ sở dữ liệu với mongoose alat
+// mongoose.connect(`mongodb+srv://truonghvph16694:${process.env.MONGO_DB}@cluster0.eux1ygq.mongodb.net/`)
+// .then(() =>{
+//     console.log('connect Db success!!');
+// })
+// .catch((err) =>{
+//     console.log('Error', err)
+// })
+
+// app.listen(port , () =>{
+//     console.log('Service is running on port', port);
+// })
