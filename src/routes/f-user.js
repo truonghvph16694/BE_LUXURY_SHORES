c257b50ca1977a0ec2cc0cf3d541d22e4a427541
Tuanvmph16643 <106154068+Tuanvmph16643@users.noreply.github.com>
2023-08-01 15:33:39 +0700

    update

--- src/routes/f-user.js
+++ src/routes/f-user.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { Create, Get, GetAll, editStatus, remove, update, signin, signup } from "../controllers/user.js";
+import { Get, GetAll, editStatus, update, signin, signup } from "../controllers/user.js";
 
 
 const router = Router();
@@ -8,8 +8,8 @@ router.post("/signup", signup);
 router.post("/signin", signin);
 router.get("/User", GetAll);
 router.get("/User/:id", Get);
-router.post("/User",Create)
+// router.post("/User",Create)
 router.put("/User/:id", update);
-router.delete("/User/:id", remove);
+// router.delete("/User/:id", remove);
 router.put("/User/status/:id", editStatus);
 export default router;
