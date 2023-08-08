0f0140d2a1c803bbeea1b8ba37007c7ff596f551
unknown <dunghtph18929@fpt.edu.vn>
2023-08-08 08:31:34 +0700

    update product

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { create, getAll, get, remove, update } from "../controllers/product";
+import { create, getAll, get, remove, update, getEdit } from "../controllers/product";
 // import { authenticate } from "../middlewares/authenticate";
 
 const router = express.Router();
 
 router.get("/products", getAll);
 router.get("/products/:id", get);
+router.get("/products-edit/:id", getEdit);
 router.post("/products", create)
-router.patch("/products/:id", update);
+router.put("/products/:id", update);
 router.delete("/products/:id", remove);
 
 export default router;      
