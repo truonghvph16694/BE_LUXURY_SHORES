7836972ce5d69c87659183854be5e08c8fb9f5cf
truonghvph16694 <96322678+truonghvph16694@users.noreply.github.com>
2023-08-30 00:37:35 +0700

    return status vnpay

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,15 +1,26 @@
-import  express  from "express";
+import express from "express";
 
-import { create, get, getAll, getProductsByCategory, remove, removeProductFromCategory, update } from "../controllers/category";
+import {
+  create,
+  get,
+  getAll,
+  getProductsByCategory,
+  remove,
+  removeProductFromCategory,
+  update,
+} from "../controllers/category";
 import { authenticate } from "../middlewares/authenticate";
 const router = express.Router();
 
-router.get("/category", authenticate, getAll);
-router.get("/category/:id", authenticate, get);
-router.get('/category/:id/products', authenticate, getProductsByCategory);
-router.post("/category" , authenticate, create);
+router.get("/category", getAll);
+router.get("/category/:id", get);
+router.get("/category/:id/products", getProductsByCategory);
+router.post("/category", authenticate, create);
 router.delete("/category/:id", authenticate, remove);
 router.put("/category/:id", authenticate, update);
-router.delete('/category/:id/products/', authenticate, removeProductFromCategory);
+router.delete(
+  "/category/:id/products/",
+  authenticate,
+  removeProductFromCategory
+);
 export default router;
-    
--- src/routes/feedback.js
+++ src/routes/feedback.js
@@ -1,16 +1,13 @@
-import  express  from "express";
+import express from "express";
 import { create, get, getAll, remove, update } from "../controllers/feedback";
 import { authenticate } from "../middlewares/authenticate";
 
-
 const router = express.Router();
 
-
-router.post("/feedback" , authenticate, create);
-router.get("/feedback" , authenticate, getAll);
-router.get("/feedback/:id" , authenticate, get);
-router.patch("/feedback/:id" , authenticate, update);
-router.delete("/feedback/:id" , authenticate, remove);
+router.post("/feedback", create);
+router.get("/feedback", getAll);
+router.get("/feedback/:id", get);
+router.patch("/feedback/:id", update);
+router.delete("/feedback/:id", authenticate, remove);
 
 export default router;
-    
